042b6651d9274d8b9163bdd586b095bf72af9736
Andrej Alpatov <alpatovandrey1980@gmail.com>
2020-11-19 23:30:36 +0100

    keep working with database

--- app/src/main/java/com/luan/hsworms/hydroid/LogoFragment.kt
+++ app/src/main/java/com/luan/hsworms/hydroid/LogoFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,6 +22,7 @@ class LogoFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -33,6 +35,7 @@ class LogoFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_logo, container, false)
     }
---
+++ app/src/main/java/com/luan/hsworms/hydroid/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.luan.hsworms.hydroid
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
