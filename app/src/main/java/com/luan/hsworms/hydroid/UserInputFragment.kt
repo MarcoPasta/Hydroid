7d835ef4c44e7b05f3ee350964a1d06d06dfa51c
Andrej Alpatov <alpatovandrey1980@gmail.com>
2020-12-04 01:43:49 +0100

    refactoring: replacing a splash screen fragment with a splash screen activity; 3 imageButtons and 2 textViews are added to the mainFragment

--- app/src/main/java/com/luan/hsworms/hydroid/UserInputFragment.kt
+++ app/src/main/java/com/luan/hsworms/hydroid/UserInputFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 class UserInputFragment : Fragment() {
 
@@ -19,6 +20,10 @@ class UserInputFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
+//        val navController = this.findNavController()
+//        navController.navigate(R.id.action_userInputFragment_to_logoFragment)
+
         return inflater.inflate(R.layout.user_input_fragment, container, false)
     }
 
