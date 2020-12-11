67a34287401ac66e0357e3ff3c01073e51ea7dc8
Andrej Alpatov <alpatovandrey1980@gmail.com>
2020-12-11 03:24:45 +0100

    Getting started with user data and saving it to a file using SharedPreferences

--- app/src/main/java/com/luan/hsworms/hydroid/MainFragment.kt
+++ app/src/main/java/com/luan/hsworms/hydroid/MainFragment.kt
@@ -1,34 +1,36 @@
 package com.luan.hsworms.hydroid
 
+import android.content.Context
+import android.content.SharedPreferences
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Observer
 import com.luan.hsworms.hydroid.databinding.MainFragmentBinding
 
 class MainFragment : Fragment() {
 
     private lateinit var binding: MainFragmentBinding
+    private lateinit var viewModel: MainViewModel
 
     companion object {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        userInput()
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate<MainFragmentBinding>(inflater,
             R.layout.main_fragment, container, false)
-
+        
         binding.changeWeather.setOnClickListener{
             val newFragment = WetterDialogFragment()
             newFragment.show(parentFragmentManager,"wetter")
@@ -39,14 +41,39 @@ class MainFragment : Fragment() {
             newFragment.show(parentFragmentManager, "sport")
         }
 
+        //Moved the initialization of view from onActivityCreated to onCreateView, since
+        // onCreateView occurs earlier and in it you can already work with variables from the view
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+
+
+        //Block of OBSERVERS to the Live Data Objects
+        //TODO: Add implementation of observer methods
+
+        viewModel.currentlyDrunkLiquid.observe(viewLifecycleOwner, { newAge ->
+            binding.textView.text = newAge.toString()
+        })
+
+        viewModel.dailyLiquidRequirement.observe(viewLifecycleOwner, { newAge ->
+
+        })
+
         return  binding.root
     }
 
+
     override fun onActivityCreated(savedInstanceState: Bundle?) {
+        Log.i("onActivityCreated", "onActivityCreated")
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
+        //Initializing an object with user data with data from a file
+        viewModel.ourUserData = activity?.getSharedPreferences(getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+
+        //The dialog is launched at the start of the application, only if the default values of the variables have not changed
+        //todo: Change the if-condition to ==
+        if(viewModel.ageOfUser.value != 0 || viewModel.weightOfUser.value != 0)
+            userInput()
     }
+
 
     //dialogFragment for entering user data
     fun userInput() {
--- app/src/main/java/com/luan/hsworms/hydroid/MainViewModel.kt
+++ app/src/main/java/com/luan/hsworms/hydroid/MainViewModel.kt
@@ -1,7 +1,31 @@
 package com.luan.hsworms.hydroid
 
+import android.content.SharedPreferences
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.currentCoroutineContext
 
 class MainViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    //TODO: Encapsulate the Live Data
+    var ageOfUser = MutableLiveData<Int>()
+    var weightOfUser = MutableLiveData<Int>()
+    var dailyLiquidRequirement = MutableLiveData<Int>()
+    var currentlyDrunkLiquid = MutableLiveData<Int>()
+    var ourUserData: SharedPreferences? = null//To save user data in internal storage
+
+
+    init {
+        ageOfUser.value = 0
+        weightOfUser.value = 0
+        dailyLiquidRequirement.value = 0
+        currentlyDrunkLiquid.value = 0
+    }
+
+
+    //Here we change the data in the storage, in case of changing the actual data
+    fun saveData(age_in: Int, weight_in: Int, liquidRequirments_in: Int, DrukLiquid_in: Int){
+        val editor = ourUserData?.edit()
+
+
+    }
 }
