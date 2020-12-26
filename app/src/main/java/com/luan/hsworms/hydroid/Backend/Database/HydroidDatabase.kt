cf11380c7d3b04860e6aea4be8f968d01579d1b2
Andrej Alpatov <alpatovandrey1980@gmail.com>
2020-12-26 03:13:29 +0100

    refactoring of DB, add layout for waterRequirement dialog

--- app/src/main/java/com/luan/hsworms/hydroid/Backend/Database/HydroidDatabase.kt
+++ app/src/main/java/com/luan/hsworms/hydroid/Backend/Database/HydroidDatabase.kt
@@ -5,21 +5,22 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [WaterRequirement::class], version = 1, exportSchema = false)
-abstract class WaterRequirementDatabase : RoomDatabase() {
+@Database(entities = [WaterRequirement::class, History::class], version = 1, exportSchema = false)
+abstract class HydroidDatabase : RoomDatabase() {
     abstract val waterRequirementDao: WaterRequirementDao
+    abstract val historyDao: HistoryDao
 
     companion object {
         @Volatile
-        private var INSTANCE: WaterRequirementDatabase? = null
+        private var INSTANCE: HydroidDatabase? = null
 
-        fun createInstance(application: Application): WaterRequirementDatabase {
+        fun createInstance(application: Application): HydroidDatabase {
             synchronized(this) {
                 var instance = INSTANCE
                 if (instance == null) {
                     instance = Room.databaseBuilder(
                         application.applicationContext,
-                        WaterRequirementDatabase::class.java,
+                        HydroidDatabase::class.java,
                         "water_requirement_database"
                     )
                         .fallbackToDestructiveMigration()
