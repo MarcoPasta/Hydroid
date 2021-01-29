90a1cdab5d6889ca2c9655ffd1b28997533c72d6
Andrej Alpatov <alpatovandrey1980@gmail.com>
2021-01-29 03:10:59 +0100

    Add functionality in SttingsFragment (change weight & gender of user)

--- app/src/main/java/com/luan/hsworms/hydroid/SettingsFragment.kt
+++ app/src/main/java/com/luan/hsworms/hydroid/SettingsFragment.kt
@@ -8,29 +8,29 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import com.google.android.material.textfield.TextInputLayout
 import com.luan.hsworms.hydroid.databinding.FragmentSettingsBinding
 
 class SettingsFragment : Fragment() {
+    private lateinit var rootView: View
 
     private lateinit var binding: FragmentSettingsBinding
     private lateinit var sharedPreferences: SharedPreferences
-    private lateinit var waterRequirements: TextView
+    private lateinit var viewModel: MainViewModel
+
+    private lateinit var rgGender: RadioGroup
+    private lateinit var etWeight: TextInputLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val context = getActivity();
-        sharedPreferences = context!!.getSharedPreferences(
-            getString(R.string.preference_file_key),
-            Context.MODE_PRIVATE
-        )
     }
 
     // lateinit var  view: View
@@ -39,24 +39,50 @@ class SettingsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        rootView = inflater.inflate(R.layout.fragment_settings, container, false)
 
-        // Inflate the layout for this fragment
-        binding = DataBindingUtil.inflate<FragmentSettingsBinding>(
-            inflater,
-            R.layout.fragment_settings,
-            container,
-            false
-        )
-        Log.i(sharedPreferences.getInt(R.string.saved_weight_of_user.toString(), 0).toString(), "Schared preference")
+        viewModel = ViewModelProvider(requireActivity(),
+            MainViewModelFactory(requireActivity().application)
+        ).get(MainViewModel::class.java)
 
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        initWidgets()
+
+//        Log.i(sharedPreferences.getInt(R.string.saved_weight_of_user.toString(), 0).toString(), "Schared preference")
+//        Log.i(viewModel.userGenderIsFemale.value.toString(), "User gender")
+        if(viewModel.userGenderIsFemale.value == true){
+            rgGender.check(R.id.rb_female)
+        } else {
+            rgGender.check(R.id.rb_male)
+        }
+
+        etWeight.editText?.setText(viewModel.weightOfUser.value.toString())
+
+        return rootView
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-                view.findViewById<TextView>(R.id.text_view_water_requirement_table)?.setOnClickListener(
+            view.findViewById<TextView>(R.id.text_view_water_requirement_table)?.setOnClickListener(
                 Navigation.createNavigateOnClickListener(R.id.action_settingsFragment_to_waterRequirementTableFragment, null)
         )
+    }
+
+    override fun onStop() {
+        super.onStop()
+        //Save Settings
+        //If gender == woman =>true else false
+        val genderSelected = view?.findViewById<RadioGroup>(R.id.rg_gender)?.checkedRadioButtonId
+        viewModel.userGenderIsFemale.value = (genderSelected == R.id.rb_female)
+        val gender = viewModel.userGenderIsFemale.value!!
+        viewModel.saveGender(gender)
+
+        val weight = view?.findViewById<TextInputLayout>(R.id.et_weight)?.editText?.text.toString().toInt()
+        viewModel.weightOfUser.value = weight
+        viewModel.saveWeight(weight)
+    }
+
+    private fun initWidgets(){
+        rgGender = rootView.findViewById(R.id.rg_gender)
+        etWeight = rootView.findViewById(R.id.et_weight)
     }
 }
---
+++ app/src/main/java/com/luan/hsworms/hydroid/SettingsViewModel.kt
@@ -0,0 +1,25 @@
+package com.luan.hsworms.hydroid
+
+import android.content.SharedPreferences
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SettingsViewModel: ViewModel() {
+
+//    //TODO: Encapsulate the Live Data
+//    var userGenderIsFemale = MutableLiveData<Boolean>()
+//    var weightOfUser = MutableLiveData<Int>()
+//    var dailyLiquidRequirement = MutableLiveData<Int>()
+//    var currentlyDrunkLiquid = MutableLiveData<Int>()
+//
+//    var ourUserData: SharedPreferences? = null      //To save user data in internal storage
+//
+//
+//    init {
+//        userGenderIsFemale.value = true
+//        weightOfUser.value = 0
+//        dailyLiquidRequirement.value = 1
+//        currentlyDrunkLiquid.value = 0
+//    }
+
+}
